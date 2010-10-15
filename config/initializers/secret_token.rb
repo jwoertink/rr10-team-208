6100e746d94afde52a8c694b7d0778d9b3c309be
Russ Smith <russ@bashme.org>
2010-10-15 16:49:31 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twithole::Application.config.secret_token = '81433b9617aad25ef3cd8397b0b6ae0474a103f95ae1aeef1eaaf7bc22cf11c569aa9f7258d8666122a3bb97e3558be43c0424ce83cf5e9b0919ca10797a4eb6'
