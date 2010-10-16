1869fd8a69b00f4129f68aed23d4e075843d419b
Russ Smith <russ@bashme.org>
2010-10-16 03:09:43 -0700

    Adding cover_me

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source "http://rubygems.org"
 gem "rails", "3.0.1"
 
 gem "capistrano"
+gem "cover_me", "1.0.0.rc2", :require => false, :group => [ :development, :test ]
 gem "haml", "3.0.21"
 gem "json", "1.4.6"
 gem "mysql2"
--- spec/models/rule_spec.rb
+++ spec/models/rule_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
 describe Rule do
-    it { should validate_presence_of(:name) }
-    it { should validate_presence_of(:description) }
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:description) }
 end
