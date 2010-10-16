4c189b566d27ef532d124f5e99370ca2520a06c5
Russ Smith <russ@bashme.org>
2010-10-15 17:53:05 -0700

    Deploy is a go.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,22 +1,32 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+require "bundler/capistrano"
 
-set :scm, :subversion
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :application, "twithole"
+set :repository, "git@github.com:railsrumble/rr10-team-208.git"
+set :deploy_to, "/u/apps/#{application}"
+set :scm, :git
+set :git_enable_submodules, 1
+set :git_shallow_clone, 1
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+set :use_sudo, false
+set :ssh_options, { :forward_agent => true }
+ssh_options[:keys] = %w(~/.ssh/twithole)
+ssh_options[:username] = "deploy"
 
-# If you are using Passenger mod_rails uncomment this:
-# if you're still using the script/reapear helper you will need
-# these http://github.com/rails/irs_process_scripts
+role :web, "173.255.195.64"
+role :app, "173.255.195.64"
+role :db,  "173.255.195.64", :primary => true
 
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+  task :start do; end
+  task :stop do; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path, "tmp", "restart.txt")}"
+  end
+
+  desc "Link in the production database.yml" 
+  task :set_database_yml do
+    run "cp #{release_path}/config/database.yml.release #{release_path}/config/database.yml" 
+  end
+end
+
+after "deploy:update_code", "deploy:set_database_yml"
