fec0b872759e2cb57d68ae975408c0bf8d742e86
Jason Arhart <jarhart@gmail.com>
2010-10-16 16:29:00 -0700

    add profile_image_url to question json

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,7 +4,7 @@ class QuestionsController < ApplicationController
   def new
     Question.take_random do |question|
       respond_with(question.to_json(
-        :only => [:category, :content, :value, :countdown],
+        :only => [:category, :content, :value, :countdown, :profile_image_url],
         :methods => [:answers, :selection]
       ))
     end
