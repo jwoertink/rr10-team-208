e33959e68c909a68bc72e58a5397dce98e5ea57c
Jason Arhart <jason@ubuntu.ubuntu-domain>
2010-10-16 04:38:20 -0700

    implement QuestionsController#show

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,9 +1,11 @@
 class QuestionsController < ApplicationController
   
   def show
+    question = Question.random
     respond_to do :format
       format.json { render :json => question }
     end
+    question.delete
   end
   
 end
