94b08f26876ae351f478f17e32da8f0dd4974650
Jason Arhart <jarhart@gmail.com>
2010-10-16 16:11:44 -0700

    add default profile image predicate to tweet model

--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -16,4 +16,27 @@ describe Tweet do
     end
   end
   
+  describe '#has_profile_image?' do
+    
+    context 'with a default profile_image_url' do
+      subject do
+        Fabricate(:tweet,
+          :profile_image_url => 'http://s.twimg.com/a/1287010001/images/default_profile_5_normal.png')
+      end
+      it 'returns false' do
+        subject.should_not have_profile_image
+      end
+    end
+    
+    context 'with a custom profile_image_url' do
+      subject do
+        Fabricate(:tweet,
+          :profile_image_url => 'http://a0.twimg.com/profile_images/1146099900/photo54_normal.jpg')
+      end
+      it 'returns true' do
+        subject.should have_profile_image
+      end
+    end
+  end
+  
 end
