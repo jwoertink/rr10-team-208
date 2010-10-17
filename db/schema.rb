b9bb1a78cb510eae2c056d8ea70d3c2ace69191e
Judd Lillestrand <juddl333@gmail.com>
2010-10-16 18:23:22 -0700

    added ISO 639 codes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101016085927) do
+ActiveRecord::Schema.define(:version => 20101017010507) do
+
+  create_table "languages", :force => true do |t|
+    t.string   "code"
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "psas", :force => true do |t|
     t.text     "text"
