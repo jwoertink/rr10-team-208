20f9b3299f7bb8e700a46cfa09a106c7b169af78
Russ Smith <russ@bashme.org>
2010-10-16 20:15:25 -0700

    We have color!!!!

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -235,6 +235,7 @@ var Game = {
 				dataType: 'json',
 				type: 'GET',
 				success: function(data) {
+				  console.log(data);
 					Game.displayQuestion(data.question);
 				},
 				error: function(XMLHttpRequest, textStatus, errorThrown) {
@@ -248,7 +249,18 @@ var Game = {
 	displayQuestion: function(question) {
 		$('.question').remove();
 		$('.splash').remove();
-		var q = $('<div class="question splash"></div>');
+
+		var color;
+		switch (question.category) {
+		case "How Many?": color = "red"; break;
+		case "Hash Tags": color = "green"; break;
+		case "True or False": color = "blue"; break;
+		case "Location": color = "purple"; break;
+		case "Pictures": color = "orange"; break;
+    }
+
+		var q = $('<div class="question splash ' + color + '"></div>');
+		q.append('<img src="' + question.profile_image_url + '" class="profile_image"/>');
 		q.append('<h2>Fix Me with a Category</h1>');
 		q.append('<h1>' + question.content + '</h1>');
 		var t = "";
@@ -316,6 +328,7 @@ var Game = {
 			type: 'GET',
 			success: function(data) {
 				var psa = $('<div class="psa splash"></div>');
+				$(psa).append('<div class="birdie"></div>');
 				$(psa).append('<h1>' + data.psa.text + '</h1><br />');
 				$(psa).append('<a href="#" rel="ready" class="button">Bring on the next question!</a>');
 				$('body').prepend(psa);
